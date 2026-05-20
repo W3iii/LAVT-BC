64e3f8cf017a7fdf4a7dcb22a12ece2549e887e4
W3iii <xushuwei1122@gmail.com>
2026-05-20 22:38:41 +0800

    modify train.sh

--- train.sh
+++ train.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-MODEL_ID=lavt_one_ln_v1
+MODEL_ID=lavt_one_ln_v1_neg05
 
 mkdir -p ./models/${MODEL_ID}
 mkdir -p ./checkpoints/
@@ -12,18 +12,18 @@ CUDA_VISIBLE_DEVICES=0 python train.py \
     --bert_tokenizer ./pretrained_weights/biobert-base-cased-v1.2 \
     --ck_bert ./pretrained_weights/biobert-base-cased-v1.2 \
     --data_root ../../groups/BME/LN_dataset_2D_vlm_2classes_neg10 \
-    --batch-size 16 \
+    --batch-size 32 \
     --lr 0.000025 \
     --wd 1e-2 \
     --swin_type base \
     --pretrained_swin_weights ./pretrained_weights/swin_base_patch4_window12_384_22k.pth \
     --window12 \
-    --epochs 50 \
+    --epochs 100 \
     --img_size 512 \
     --workers 4 \
     --pin_mem \
     --output-dir ./checkpoints/${MODEL_ID} \
-    --neg_ratio 0.3 \
-    --n_soft_tokens 4 \
+    --neg_ratio 0.5 \
+    --n_soft_tokens 6 \
     --seed 42 \
     2>&1 | tee ./models/${MODEL_ID}/output
